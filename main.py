a958a95ee884ad54410f93b7b87775c78b845fc2
Pierre Rochard <pierre@rochard.org>
2015-12-24 17:29:33 -0500

    refactoring

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ from quamash import QEventLoop, QThreadExecutor
 
 
 @asyncio.coroutine
-def master(window):
-    yield from first_50(window)
+def master(progress):
+    yield from first_50(progress)
     loop = asyncio.get_event_loop()
     with QThreadExecutor(1) as executor:
-        yield from loop.run_in_executor(executor, functools.partial(last_50, window, loop))
+        yield from loop.run_in_executor(executor, functools.partial(last_50, progress, loop))
 
 
 @asyncio.coroutine
-def first_50(window):
+def first_50(progress):
     for i in range(50):
-        window.progress.setValue(i)
+        progress.setValue(i)
         yield from asyncio.sleep(.1)
 
 
-def last_50(window, loop):
+def last_50(progress, loop):
     for i in range(50, 100):
-        loop.call_soon_threadsafe(window.progress.setValue, i)
+        loop.call_soon_threadsafe(progress. setValue, i)
         time.sleep(.1)
 
 
-class Example(QtGui.QWidget):
+class MainWindow(QtGui.QWidget):
     def __init__(self):
-        super(Example, self).__init__()
+        super(MainWindow, self).__init__()
         self.progress = QtGui.QProgressBar(self)
         self.progress.setRange(0, 99)
         self.setGeometry(300, 300, 250, 150)
@@ -42,9 +42,9 @@ def main():
     app = QtGui.QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
-    window = Example()
+    main_window = MainWindow()
     with loop:
-        loop.run_until_complete(master(window))
+        loop.run_until_complete(master(main_window.progress))
     sys.exit(app.exec_())
 
 if __name__ == '__main__':

6fcbc5b5b4b2de6c8a9fa2a99c156cd1ca393902
Pierre Rochard <pierre@rochard.org>
2015-12-25 12:08:15 -0500

    experiments

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import warnings
 import numpy as np
 from PyQt4 import QtGui, uic
 from quamash import QEventLoop, QThreadExecutor
+from vispy import plot as vp
+from vispy.app.qt import QtSceneCanvas
 from vispy.scene import visuals
+from vispy.scene.subscene import SubScene
+from vispy.util import load_data_file
+from vispy import gloo, keys, app
 
 
 @asyncio.coroutine
@@ -22,13 +27,13 @@ def master(progress):
 def first_50(progress):
     for i in range(50):
         progress.setValue(i)
-        yield from asyncio.sleep(.1)
+        yield from asyncio.sleep(.01)
 
 
 def last_50(progress, loop):
     for i in range(50, 100):
         loop.call_soon_threadsafe(progress.setValue, i)
-        time.sleep(.1)
+        time.sleep(.01)
 
 with warnings.catch_warnings(record=True):
     WindowTemplate, TemplateBaseClass = uic.loadUiType('qt-designer.ui')
@@ -40,8 +45,20 @@ with warnings.catch_warnings(record=True):
             self.ui = WindowTemplate()
             self.ui.setupUi(self)
             self.ui.progressBar.setRange(0, 99)
-            data = np.linspace(0, 100)
-            visuals.LinePlot(data=data, parent=self.ui.canvas.scene)
+            self.ui.canvas = MainCanvas(self.ui.frame)
+            # @self.ui.canvas.connect
+            # def on_resize(event):
+            #     gloo.set_viewport(0, 0, *event.size)
+
+
+class MainCanvas(QtSceneCanvas):
+    def __init__(self, parent):
+        super(QtSceneCanvas, self).__init__(parent)
+        self.scene = SubScene()
+        data = np.linspace(0, 100)
+        visuals.LinePlot(data=data, parent=self.scene)
+    def on_resize(self, event):
+        self.apply_zoom()
 
 
 def main():

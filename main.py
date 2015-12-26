07fde723a79a9e86019fd2e65ea4ad45ab5f43a8
Pierre Rochard <pierre@rochard.org>
2015-12-26 09:49:49 -0500

    experiments

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import asyncio
 import functools
 import json
 import sys
-import time
 import warnings
+from datetime import datetime
 
 import numpy as np
+import requests
 from PyQt4 import QtGui, uic
 from quamash import QEventLoop
 from vispy import gloo, app
 import websockets
-from vispy.plot import PlotWidget
-from vispy.scene import visuals, SceneCanvas
 
 
 @asyncio.coroutine
@@ -23,10 +22,12 @@ def master(main_window):
         message = yield from coinbase_websocket.recv()
         message = json.loads(message)
         main_window.ui.sequence_label.setText('Sequence: {0}'.format(message['sequence']))
+        if message['type'] == 'match':
+            main_window.ui.canvas.match_prices += [float(message['price'])]
+            main_window.ui.canvas.match_dates += [datetime.strptime(message['time'], '%Y-%m-%dT%H:%M:%S.%fZ')]
 
 with warnings.catch_warnings(record=True):
     WindowTemplate, TemplateBaseClass = uic.loadUiType('qt-designer.ui')
-
 
     class MainWindow(TemplateBaseClass):
         def __init__(self):
@@ -46,6 +47,12 @@ with warnings.catch_warnings(record=True):
 class MainCanvas(app.Canvas):
     def __init__(self, parent):
         app.Canvas.__init__(self, keys='interactive', parent=parent)
+        self.match_dates = []
+        self.match_prices = []
+        matches = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/trades').json()
+        for match in matches:
+            self.match_dates += [datetime.strptime(match['time'], '%Y-%m-%dT%H:%M:%S.%fZ')]
+            self.match_prices += [float(match['price'])]
         vertex = """
         attribute vec2 a_position;
         void main (void)
@@ -64,11 +71,13 @@ class MainCanvas(app.Canvas):
         self.program['a_position'] = np.c_[
             np.linspace(-1.0, +1.0, 1000),
             np.random.uniform(-0.5, +0.5, 1000)].astype(np.float32)
-        print(np.c_[
-            np.linspace(-1.0, +1.0, 1000),
-            np.random.uniform(-0.5, +0.5, 1000)].astype(np.float32))
-        self._timer = app.Timer('auto', connect=self.on_timer_event,
-                                start=True)
+
+        # print(np.c_[np.array(])].astype(np.float32))
+        self._timer = app.Timer('auto', connect=self.on_timer_event, start=True)
+
+    # def calculate_plot(self):
+
+
 
     def on_timer_event(self, event):
         self.program['a_position'] = np.c_[

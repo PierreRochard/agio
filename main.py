b7ea83990f55fe0c676e4c8c9d6d8664f89bd35a
Pierre Rochard <pierre@rochard.org>
2015-12-27 21:19:16 -0500

    switching away from asyncio

--- main.py
+++ main.py
@@ -6,25 +6,59 @@ import warnings
 from datetime import datetime
 
 import numpy as np
+import pytz
 import requests
 from PyQt4 import QtGui, uic
-from quamash import QEventLoop
+from PyQt4.QtGui import QListWidgetItem, QFont
+from dateutil.tz import tzlocal
+from quamash import QEventLoop, QtCore
 from vispy import gloo, app
 import websockets
+import websocket
 
 
-@asyncio.coroutine
-def master(main_window):
-    main_window.websocket = True
-    coinbase_websocket = yield from websockets.connect("wss://ws-feed.exchange.coinbase.com")
-    yield from coinbase_websocket.send('{"type": "subscribe", "product_id": "BTC-USD"}')
-    while main_window.websocket:
-        message = yield from coinbase_websocket.recv()
+# @asyncio.coroutine
+# def master(main_window):
+#     main_window.websocket = True
+#     coinbase_websocket = yield from websockets.connect("wss://ws-feed.exchange.coinbase.com")
+#     yield from coinbase_websocket.send('{"type": "subscribe", "product_id": "BTC-USD"}')
+#     while main_window.websocket:
+#         message = yield from coinbase_websocket.recv()
+#         message = json.loads(message)
+#         main_window.ui.sequence_label.setText('Sequence: {0}'.format(message['sequence']))
+#         if message['type'] == 'match':
+#             main_window.ui.canvas.match_prices += [float(message['price'])]
+#             main_window.ui.canvas.match_dates += [datetime.strptime(message['time'], '%Y-%m-%dT%H:%M:%S.%fZ')]
+
+class ListenWebsocket(QtCore.QThread):
+    def __init__(self, main_window=None):
+        super(ListenWebsocket, self).__init__(main_window)
+        self.main_window = main_window
+        self.WS = websocket.WebSocketApp("wss://ws-feed.exchange.coinbase.com",
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+
+    def run(self):
+        self.WS.on_open = self.on_open
+
+        self.WS.run_forever()
+
+    def on_open(self, ws):
+        self.WS.send('{"type": "subscribe", "product_id": "BTC-USD"}')
+
+    def on_message(self, ws, message):
         message = json.loads(message)
-        main_window.ui.sequence_label.setText('Sequence: {0}'.format(message['sequence']))
+        self.main_window.ui.sequence_label.setText('Sequence: {0}'.format(message['sequence']))
         if message['type'] == 'match':
-            main_window.ui.canvas.match_prices += [float(message['price'])]
-            main_window.ui.canvas.match_dates += [datetime.strptime(message['time'], '%Y-%m-%dT%H:%M:%S.%fZ')]
+            self.main_window.add_match(message)
+
+    def on_error(self, ws, error):
+        print(error)
+
+    def on_close(self, ws):
+        return True
+
 
 with warnings.catch_warnings(record=True):
     WindowTemplate, TemplateBaseClass = uic.loadUiType('qt-designer.ui')
@@ -34,23 +68,32 @@ with warnings.catch_warnings(record=True):
             TemplateBaseClass.__init__(self)
             self.ui = WindowTemplate()
             self.ui.setupUi(self)
-            self.ui.canvas = MainCanvas(self.ui.canvas)
-            loop = asyncio.get_event_loop()
-            self.websocket = False
-            self.ui.start_button.clicked.connect(lambda: loop.run_until_complete(master(self)))
-            self.ui.stop_button.clicked.connect(functools.partial(self.toggle_websocket, False))
+            self.thread = ListenWebsocket(self)
+            self.matches = []
+            print(self.matches)
+            self.ui.start_button.clicked.connect(functools.partial(self.thread.start))
+            start_matches = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/trades').json()
+            for message in reversed(start_matches):
+                self.add_match(message)
+            # self.ui.canvas = MainCanvas(self.ui.canvas, self)
 
-        def toggle_websocket(self, status):
-            self.websocket = status
+        def add_match(self, message):
+            self.matches += [message]
+            size = '{0:.4f}'.format(float(message['size']))
+            timestamp = datetime.strptime(message['time'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=pytz.UTC)
+            while len(size) < 8:
+                size = ' ' + size
+            item = QListWidgetItem('{0} {1:.2f} {2}'.format(size, float(message['price']), timestamp.astimezone(tzlocal()).strftime('%H:%M:%S.%f')))
+            item.setFont(QFont('Courier New'))
+            self.ui.matches_list.insertItem(-1, item)
 
 
 class MainCanvas(app.Canvas):
-    def __init__(self, parent):
+    def __init__(self, parent, main_window):
         app.Canvas.__init__(self, keys='interactive', parent=parent)
         self.match_dates = []
         self.match_prices = []
-        matches = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/trades').json()
-        for match in matches:
+        for match in main_window.matches:
             self.match_dates += [datetime.strptime(match['time'], '%Y-%m-%dT%H:%M:%S.%fZ')]
             self.match_prices += [float(match['price'])]
         vertex = """
@@ -76,8 +119,6 @@ class MainCanvas(app.Canvas):
         self._timer = app.Timer('auto', connect=self.on_timer_event, start=True)
 
     # def calculate_plot(self):
-
-
 
     def on_timer_event(self, event):
         self.program['a_position'] = np.c_[
